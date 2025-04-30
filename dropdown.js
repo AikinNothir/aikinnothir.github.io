bd5946dd9a1e08120b0b79ed0c8d2df7597341fd
AikinNothir <jndr.dusek@gmail.com>
2025-04-30 18:40:54 +0200

    sidenav dropdown zkrácen

--- dropdown.js
+++ dropdown.js
@@ -2,14 +2,11 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "none") {
-      dropdownContent.style.display = "block";
-    } else {
-      dropdownContent.style.display = "none";
-    }
-  });
-}
+dropdown.addEventListener("click", function () {
+  var dropdownContent = document.getElementsByClassName("dropdown-div");
+  if (dropdownContent.style.display === "none") {
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
+});
