bc29aa4d117e7098938ef8d20024916accbf7526
AikinNothir <jndr.dusek@gmail.com>
2025-04-30 17:30:25 +0200

    button v sidenav lol vymazano <i>

--- sidenav.js
+++ sidenav.js
@@ -14,9 +14,7 @@ function napisSidebar() {
             <a href="/stark.html" class="postava">Stark</a> 
             <a href="/zuana.html" class="postava">Zuana</a>  
             <br>
-            <button class="dropdown-btn">NPC
-                <i class="fa fa-caret-down"></i>
-            </button>
+            <button class="dropdown-btn">NPC</button>
             <div class="dropdown-div">
             <a href="/NPC/neera.html" class="postava">Neera</a> 
             <a href="/NPC/narmer.html" class="postava">Narmer</a>
