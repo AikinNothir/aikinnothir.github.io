498b0043ce80f6b57cfeeab41b0a3add1b9c31ff
AikinNothir <jndr.dusek@gmail.com>
2025-05-01 16:16:54 +0200

    === na =

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,6 @@
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 function rozbal(idDivu) {
-  if (document.getElementById(idDivu).style.display === "block") {
+  if ((document.getElementById(idDivu).style.display = "block")) {
     document.getElementById(idDivu).style.display = "none";
   } else {
     document.getElementById(idDivu).style.display = "block";
