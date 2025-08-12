c2a3747899e25f2a6f7cb42d7b722841d9c51be1
aikinnothir <jindra.aikin@gmail.com>
2025-08-12 12:13:18 +0200

    w33 class misto style width 33

--- header.js
+++ header.js
@@ -4,11 +4,11 @@ function napisHeader() {
   ).innerHTML = `<div style="text-align:left;margin-left:12px;width:18%;align-content: center;justify-content: center;">
         <span style="font-size:30px;cursor:pointer;" onclick="openNav();napisSidebar()">&#9776; </span>
     </div>
-    <div class="logo" style="width:33%;">
+    <div class="logo w33">
                     <a href="/index.html" class="logo">
                         DRUŽINA
                     </a>
                 </div>
-                <div style="width:33%">
+                <div class="w33">
                 </div>`;
 }
