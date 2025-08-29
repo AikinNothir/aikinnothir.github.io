b52b08651695a6cf16c7b5f33c5f6284563f7a64
aikinnothir <jindra.aikin@gmail.com>
2025-08-29 11:12:02 +0200

    mort

--- sidenav.js
+++ sidenav.js
@@ -154,3 +154,16 @@ document.querySelectorAll('.dropdown-btn').forEach((d) => {
     });
   });
 }
+
+function rozbal(idDivu) {
+  const tenDiv = document.getElementById(idDivu);
+  let aktVisibility = getComputedStyle(tenDiv).visibility;
+
+  if (aktVisibility === "hidden") {
+    tenDiv.style.visibility = "visible";
+    tenDiv.style.maxHeight = "2000px";
+  } else {
+    tenDiv.style.visibility = "hidden";
+    tenDiv.style.maxHeight = "0px";
+  };
+}
