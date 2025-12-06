821882e3b5cff8df0feffb38e62deac0ece04e0d
AikinNothir <jndr.dusek@gmail.com>
2025-12-06 15:36:12 +0100

    nad danej jen do scriptu

---
+++ nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mysidenav").style.width = "290px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mysidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
