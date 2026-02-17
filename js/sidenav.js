93e7be7417ee08624d79ed716b40ddb0e3121c3b
AikinNothir <jndr.dusek@gmail.com>
2026-02-17 23:15:58 +0100

    nova navigace

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,30 +1,35 @@
+/*<button class="dropdown-btn" onclick="rozbal('lokVar')">
+  Varka
+  <span><i class="arrow down"></i></span>
+</button>
+<div class="dropdown-div" id="lokVar" style="visibility:hidden;max-height:0px">
+  
+</div>*/
+
 function napisSidebar() {
   document.getElementById("SIDE").innerHTML =
     `<a href="/index.html" class="postava azure"><h2>DRUŽINA</h2></a> 
-            <h2> Postavy </h2>
-            <hr class="solid">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="/postavy/brom.html" class="postava">Brom</a> 
-            <a href="/postavy/emer.html" class="postava">Emer z Dorinu</a>
-            <a href="/postavy/garth.html" class="postava">Garth McLaughlin</a>
-            <a href="/postavy/chloe.html" class="postava" id="mort">Chloe</a> 
-            <a href="/postavy/joe.html" class="postava">Joe</a>
-            <a href="/postavy/joli.html" class="postava">Joli</a>
-            <a href="/postavy/kharak.html" class="postava">Kharak Narantl</a> 
-            <a href="/postavy/pertan.html" class="postava">Pertan Perlík</a> 
-            <a href="/postavy/sirael.html" class="postava">Sirael Cockven</a>
-            <a href="/postavy/zuana.html" class="postava">Zuana</a>  
+            <button class="dropdown-btn" onclick="rozbal('postavy')">
+              Postavy
+              <span><i class="arrow down"></i></span>
+            </button>
+            <div class="dropdown-div" id="postavy" style="visibility:hidden;max-height:0px">
+              <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+              <a href="/postavy/brom.html" class="postava">Brom</a> 
+              <a href="/postavy/emer.html" class="postava">Emer z Dorinu</a>
+              <a href="/postavy/garth.html" class="postava">Garth McLaughlin</a>
+              <a href="/postavy/chloe.html" class="postava" id="mort">Chloe</a> 
+              <a href="/postavy/joe.html" class="postava">Joe</a>
+              <a href="/postavy/joli.html" class="postava">Joli</a>
+              <a href="/postavy/kharak.html" class="postava">Kharak Narantl</a> 
+              <a href="/postavy/pertan.html" class="postava">Pertan Perlík</a> 
+              <a href="/postavy/sirael.html" class="postava">Sirael Cockven</a>
+              <a href="/postavy/zuana.html" class="postava">Zuana</a>  
+            </div>
             <br>
-            <a href="/mapa.html" class="mapa">Interaktivní Mapa</a>
-            <a href="/mapabig.html" class="mapa">VELKÁ MAPA!</a>
-            <a href="/cheatsheet.html" class="mapa">Cheatsheet</a>
-            <a href="/denik.pdf" class="mapa" download>Deník!</a>
-            <br>
-
             <button class="dropdown-btn" onclick="rozbal('div1')">NPC 
               <span><i class="arrow down"></i></span>
             </button>
-            <hr class="solid">
             <div class="dropdown-div" id="div1" style="visibility:hidden;max-height:0px">
               <a href="/npc/bestie.html" class="postava">Bestie</a>
               <a href="/npc/dan.html" class="postava">Dan</a>
@@ -49,50 +54,61 @@ function napisSidebar() {
             </div>
             <br>
 
-            <button class="dropdown-btn" onclick="rozbal('div2')">Místa
+            <button class="dropdown-btn" onclick="rozbal('div2')">
+              Místa
                 <span><i class="arrow down"></i></span>
             </button>
-            <hr class="solid">
             <div class="dropdown-div" id="div2" style="visibility:hidden;max-height:0px">
-              <a target="_self" alt="Adanosův chrám" class="misto" href="/mista/adanos/adanos.html">Adanosův chrám</a>
-              <a target="_self" alt="Ruiny Al Katu" class="misto" href="/mista/al_kat/al_kat.html">Al-Kat</a>
+              <button class="dropdown-btn2" onclick="rozbal('lokVar')">
+                Varka
+                <span><i class="arrow down"></i></span>
+              </button>
+              <div class="dropdown-div2" id="lokVar" style="visibility:hidden;max-height:0px">
+                <a target="_self" alt="Adanosův chrám" class="misto" href="/mista/adanos/adanos.html">Adanosův chrám</a>
+                <a target="_self" alt="Ruiny Al Katu" class="misto" href="/mista/al_kat/al_kat.html">Al-Kat</a>
+                <a target="_self" alt="Durnhelm" class="misto" href="/mista/durnhelm/durnhelm.html">Durnhelm</a>
+                <a target="_self" alt="Garagor" class="misto" href="/mista/garagor/garagor.html">Garagor</a> 
+                <a target="_self" alt="Ghetto" class="misto" href="/mista/ghetto/ghetto.html">Ghetto</a>
+                <a target="_self" alt="Les Gost" class="misto" href="/mista/gost/gost.html">Gost</a> 
+                <a target="_self" alt="Grabštejn" class="misto" href="/mista/grabstejn/grabstejn.html">Grabštejn</a>
+                <a target="_self" alt="Hornická Oblast" class="misto" href="/mista/doly/doly.html">Hornická oblast</a>
+                <a target="_self" alt="Horská pevnost" class="misto" href="/mista/horska_pevnost/horska_pevnost.html">Horská pevnost</a>
+                <a target="_self" alt="Chvozd" class="misto" href="/mista/chvozd/chvozd.html">Chvozd</a>
+                <a target="_self" alt="Ječín" class="misto" href="/mista/jecin/jecin.html">Ječín</a>
+                <a target="_self" alt="Kriegberg" class="misto" href="/mista/kriegberg/kriegberg.html">Kriegberg</a>
+                <a target="_self" alt="Kriegstein" class="misto" href="/mista/kriegstein/kriegstein.html">Kriegstein</a>
+                <a target="_self" alt="Les Mrtvých Vesnic" class="misto" href="/mista/les_mrtvych_vesnic/les_mrtvych_vesnic.html">Les Mrtvých Vesnic</a>
+                <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz </a>
+                <a target="_self" alt="Morgstadt" class="misto" href="/mista/morgstadt/morgstadt.html">Morgstadt</a>
+                <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz</a>
+                <a target="_self" alt="Pole Smutku" class="misto" href="/mista/pole_smutku/pole_smutku.html">Pole Smutku</a>
+                <a target="_self" alt="Pons Varoli" class="misto" href="/mista/pons/pons.html">Pons Varoli</a>
+                <a target="_self" alt="Přechod" class="misto" href="/mista/prechod/prechod.html">Přechod</a>
+                <a target="_self" alt="Pustina" class="misto" href="/mista/pustina/pustina.html">Pustina</a>
+                <a target="_self" alt="Sucholes" class="misto" href="/mista/sucholes/sucholes.html">Sucholes</a>
+                <a target="_self" alt="Trombus" class="misto" href="/mista/trombus/trombus.html">Trombus</a>
+                <a target="_self" alt="Trpasličí Zátoka" class="misto" href="/mista/zatoka/zatoka.html">Trpasličí Zátoka</a>
+                <a target="_self" alt="Varka" class="misto" href="/mista/varka/varka.html">Varka</a>
+                <a target="_self" alt="Věž" class="misto" href="/mista/vez/vez.html">Věž</a>
+              </div>
+              <button class="dropdown-btn2" onclick="rozbal('lokLar')">
+                Laréze
+                <span><i class="arrow down"></i></span>
+              </button>
+              <div class="dropdown-div2" id="lokLar" style="visibility:hidden;max-height:0px">
+                <a target="_self" alt="Hayax" class="misto" href="/mista/hayax/hayax.html">Hayax</a>
+                <a target="_self" alt="Laréze" class="misto" href="/mista/lareze/lareze.html">Laréze</a>
+                <a target="_self" alt="Mýtinná" class="misto" href="/mista/mytinna/mytinna.html">Mýtinná</a>
+                <a target="_self" alt="Všehostrom" class="misto" href="/mista/vsehostrom/vsehostrom.html">Všehostrom</a>
+              </div>
               <a target="_self" alt="Denthir" class="misto" href="/mista/denthir/denthir.html">Denthir</a>
-              <a target="_self" alt="Durnhelm" class="misto" href="/mista/durnhelm/durnhelm.html">Durnhelm</a>
-              <a target="_self" alt="Garagor" class="misto" href="/mista/garagor/garagor.html">Garagor</a> 
-              <a target="_self" alt="Ghetto" class="misto" href="/mista/ghetto/ghetto.html">Ghetto</a>
-              <a target="_self" alt="Les Gost" class="misto" href="/mista/gost/gost.html">Gost</a> 
-              <a target="_self" alt="Grabštejn" class="misto" href="/mista/grabstejn/grabstejn.html">Grabštejn</a>
-              <a target="_self" alt="Hayax" class="misto" href="/mista/hayax/hayax.html">Hayax</a>
-              <a target="_self" alt="Hornická Oblast" class="misto" href="/mista/doly/doly.html">Hornická oblast</a>
-              <a target="_self" alt="Horská pevnost" class="misto" href="/mista/horska_pevnost/horska_pevnost.html">Horská pevnost</a>
-              <a target="_self" alt="Chvozd" class="misto" href="/mista/chvozd/chvozd.html">Chvozd</a>
-              <a target="_self" alt="Ječín" class="misto" href="/mista/jecin/jecin.html">Ječín</a>
-              <a target="_self" alt="Kriegberg" class="misto" href="/mista/kriegberg/kriegberg.html">Kriegberg</a>
-              <a target="_self" alt="Kriegstein" class="misto" href="/mista/kriegstein/kriegstein.html">Kriegstein</a>
-              <a target="_self" alt="Laréze" class="misto" href="/mista/lareze/lareze.html">Laréze</a>
               <a target="_self" alt="Liedemar" class="misto" href="/mista/liedemar/liedemar.html">Liedemar</a>
-              <a target="_self" alt="Les Mrtvých Vesnic" class="misto" href="/mista/les_mrtvych_vesnic/les_mrtvych_vesnic.html">Les Mrtvých Vesnic</a>
-              <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz </a>
-              <a target="_self" alt="Morgstadt" class="misto" href="/mista/morgstadt/morgstadt.html">Morgstadt</a>
-              <a target="_self" alt="Mýtinná" class="misto" href="/mista/mytinna/mytinna.html">Mýtinná</a>
-              <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz</a>
-              <a target="_self" alt="Pole Smutku" class="misto" href="/mista/pole_smutku/pole_smutku.html">Pole Smutku</a>
-              <a target="_self" alt="Pons Varoli" class="misto" href="/mista/pons/pons.html">Pons Varoli</a>
-              <a target="_self" alt="Přechod" class="misto" href="/mista/prechod/prechod.html">Přechod</a>
-              <a target="_self" alt="Pustina" class="misto" href="/mista/pustina/pustina.html">Pustina</a>
-              <a target="_self" alt="Sucholes" class="misto" href="/mista/sucholes/sucholes.html">Sucholes</a>
-              <a target="_self" alt="Trombus" class="misto" href="/mista/trombus/trombus.html">Trombus</a>
-              <a target="_self" alt="Trpasličí Zátoka" class="misto" href="/mista/zatoka/zatoka.html">Trpasličí Zátoka</a>
-              <a target="_self" alt="Varka" class="misto" href="/mista/varka/varka.html">Varka</a>
-              <a target="_self" alt="Věž" class="misto" href="/mista/vez/vez.html">Věž</a>
-              <a target="_self" alt="Všehostrom" class="misto" href="/mista/vsehostrom/vsehostrom.html">Všehostrom</a>
             </div>
             <br>
 
             <button class="dropdown-btn" onclick="rozbal('div3')">Frakce
                 <span><i class="arrow down"></i></span>
             </button>
-            <hr class="solid">
             <div class="dropdown-div" style="visibility:hidden;max-height:0px" id="div3">
               <a target="_self" alt="Aloca" class="misto" href="/frakce/aloca.html">Aloca</a>
               <a target="_self" alt="Alchymistický cech" class="misto" href="/frakce/alchym_cech.html">Alchymistický cech</a>
@@ -108,7 +124,6 @@ function napisSidebar() {
             <button class="dropdown-btn" onclick="rozbal('div4')">Bohové 
               <span><i class="arrow down"></i></span>
             </button>
-            <hr class="solid">
             <div class="dropdown-div" id="div4" style="visibility:hidden;max-height:0px">
               <a href="/bohove/aaauph.html" class="postava">Aaauph</a>
               <a href="/bohove/adanos.html" class="postava">Adanos</a>
@@ -131,7 +146,6 @@ function napisSidebar() {
             <button class="dropdown-btn" onclick="rozbal('divSpell')">Spellbook
                 <span><i class="arrow down"></i></span>
             </button>
-            <hr class="solid">
             <div class="dropdown-div" id="divSpell" style="visibility:hidden;max-height:0px">
               <a href="https://dnd5e.wikidot.com/spells:druid" class="postava">Druid</a>
               <a href="https://dnd5e.wikidot.com/spells:cleric" class="postava">Klerik</a>  
@@ -140,6 +154,12 @@ function napisSidebar() {
               <a href="https://dnd5e.wikidot.com/spells:ranger" class="postava">Ranger</a> 
               <a href="/Spellbook/fighterSb.html" class="postava">Válečník</a> 
             </div>
+            <br>
+            <a href="/mapa.html" class="mapa">Interaktivní Mapa</a>
+            <a href="/mapabig.html" class="mapa">VELKÁ MAPA!</a>
+            <a href="/cheatsheet.html" class="mapa">Cheatsheet</a>
+            <a href="/denik.pdf" class="mapa" download>Deník!</a>
+            <br>
             <br>
             <a href="/info.html" class="postava">O stránce</a> 
             `;
