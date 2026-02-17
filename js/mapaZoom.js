4efbb99842b8707b83c778dc8653128a9e0fbb53
aikinnothir <jindra.aikin@gmail.com>
2026-02-17 15:23:58 +0100

    javascript slozka + mapaZoom.js update - na tom jeste bude prace

---
+++ js/mapaZoom.js
@@ -0,0 +1,124 @@
+function magnify(imgID, zoom) {
+  const img = document.getElementById(imgID);
+  const glass = document.createElement("div");
+  glass.className = "img-magnifier-glass";
+  img.parentElement.insertBefore(glass, img);
+
+  glass.style.backgroundImage = `url('${img.src}')`;
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = `${img.width * zoom}px ${img.height * zoom}px`;
+
+  const bw = 3;
+  const w = glass.offsetWidth / 2;
+  const h = glass.offsetHeight / 2;
+
+  const rect = img.getBoundingClientRect(); // jen jednou!
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let ticking = false;
+
+  function updateGlass() {
+    let x = mouseX;
+    let y = mouseY;
+
+    if (x > img.width - (w / zoom)) x = img.width - (w / zoom);
+    if (x < w / zoom) x = w / zoom;
+    if (y > img.height - (h / zoom)) y = img.height - (h / zoom);
+    if (y < h / zoom) y = h / zoom;
+
+    // GPU friendly
+    glass.style.transform = `translate(${x - w}px, ${y - h}px)`;
+
+    glass.style.backgroundPosition =
+      `-${(x * zoom) - w + bw}px -${(y * zoom) - h + bw}px`;
+
+    ticking = false;
+  }
+
+  function moveMagnifier(e) {
+    e.preventDefault();
+
+    mouseX = e.pageX - rect.left - window.pageXOffset;
+    mouseY = e.pageY - rect.top - window.pageYOffset;
+
+    if (!ticking) {
+      requestAnimationFrame(updateGlass);
+      ticking = true;
+    }
+  }
+
+  img.addEventListener("mousemove", moveMagnifier);
+  glass.addEventListener("mousemove", moveMagnifier);
+}
+
+
+/* document.querySelectorAll("a.img-magnifier-container").forEach(acko => {
+    acko.addEventListener("click", function(prA) {
+        prA.preventDefault();
+        console.log("Klikl jsi na mapu! Nech toho!")
+    });
+});
+
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  // Create magnifier glass: 
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+
+  // Insert magnifier glass: 
+  img.parentElement.insertBefore(glass, img);
+
+  // Set background properties for the magnifier glass: 
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+
+  console.log(img.width + " " + img.height + " " + zoom )
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  // Execute a function when someone moves the magnifier glass over the image: 
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  //and also for touch screens:
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
+    var pos, x, y;
+    // Prevent any other actions that may occur when moving over the image 
+    e.preventDefault();
+    // Get the cursor's x and y positions: 
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    // Prevent the magnifier glass from being positioned outside the image: 
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    // Set the position of the magnifier glass: 
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    // Display what the magnifier glass "sees": 
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    // Get the x and y positions of the image: 
+    a = img.getBoundingClientRect();
+    // Calculate the cursor's x and y coordinates, relative to the image: 
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    // Consider any page scrolling: 
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+} */
---
+++ js/sidenav.js
@@ -0,0 +1,180 @@
+function napisSidebar() {
+  document.getElementById("SIDE").innerHTML =
+    `<a href="/index.html" class="postava azure"><h2>DRUŽINA</h2></a> 
+            <h2> Postavy </h2>
+            <hr class="solid">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="/postavy/brom.html" class="postava">Brom</a> 
+            <a href="/postavy/emer.html" class="postava">Emer z Dorinu</a>
+            <a href="/postavy/garth.html" class="postava">Garth McLaughlin</a>
+            <a href="/postavy/chloe.html" class="postava" id="mort">Chloe</a> 
+            <a href="/postavy/joe.html" class="postava">Joe</a>
+            <a href="/postavy/joli.html" class="postava">Joli</a>
+            <a href="/postavy/kharak.html" class="postava">Kharak Narantl</a> 
+            <a href="/postavy/pertan.html" class="postava">Pertan Perlík</a> 
+            <a href="/postavy/sirael.html" class="postava">Sirael Cockven</a>
+            <a href="/postavy/zuana.html" class="postava">Zuana</a>  
+            <br>
+            <a href="/mapa.html" class="mapa">Interaktivní Mapa</a>
+            <a href="/mapabig.html" class="mapa">VELKÁ MAPA!</a>
+            <a href="/cheatsheet.html" class="mapa">Cheatsheet</a>
+            <a href="/denik.pdf" class="mapa" download>Deník!</a>
+            <br>
+
+            <button class="dropdown-btn" onclick="rozbal('div1')">NPC 
+              <span><i class="arrow down"></i></span>
+            </button>
+            <hr class="solid">
+            <div class="dropdown-div" id="div1" style="visibility:hidden;max-height:0px">
+            <a href="/npc/bestie.html" class="postava">Bestie</a>
+            <a href="/npc/dan.html" class="postava">Dan</a>
+            <a href="/npc/darub.html" class="postava">Darub</a>
+            <a href="/npc/dernhelm.html" class="postava">Dernhelm</a> 
+            <a href="/npc/elisa.html" class="postava">Elisa</a>
+            <a href="/npc/enea.html" class="postava">Enea Mara</a>
+            <a href="/postavy/folgrim.html" class="postava">Folgrim</a>  
+            <a href="/npc/hugo.html" class="postava">Hugo</a>
+            <a href="/npc/gellis.html" class="postava">Gellis</a>
+            <a href="/npc/ignus.html" class="postava">Ignus</a>
+            <a href="/npc/koril.html" class="postava">Koril</a>
+            <a href="/npc/merion.html" class="postava">Merion</a>
+            <a href="/npc/motieur.html" class="postava">Motieur</a>  
+            <a href="/npc/narmer.html" class="postava">Narmer</a>
+            <a href="/npc/neera.html" class="postava">Neera</a>
+            <a href="/npc/sgreti.html" class="postava">Sgréti</a>
+            <a href="/npc/tibalt.html" class="postava">Tibalt</a>  
+            <a href="/npc/tonik.html" class="postava">Toník</a> 
+            <a href="/npc/treti.html" class="postava">Třetí</a>  
+            <a href="/npc/zrzek.html" class="postava">Zrzek</a>  
+            </div>
+            <br>
+
+            <button class="dropdown-btn" onclick="rozbal('div2')">Místa
+                <span><i class="arrow down"></i></span>
+            </button>
+            <hr class="solid">
+            <div class="dropdown-div" id="div2" style="visibility:hidden;max-height:0px">
+                <a target="_self" alt="Adanosův chrám" class="misto" href="/mista/adanos/adanos.html">Adanosův chrám</a>
+                <a target="_self" alt="Ruiny Al Katu" class="misto" href="/mista/al_kat/al_kat.html">Al-Kat</a>
+                <a target="_self" alt="Denthir" class="misto" href="/mista/denthir/denthir.html">Denthir</a>
+                <a target="_self" alt="Durnhelm" class="misto" href="/mista/durnhelm/durnhelm.html">Durnhelm</a>
+                <a target="_self" alt="Garagor" class="misto" href="/mista/garagor/garagor.html">Garagor</a> 
+                <a target="_self" alt="Ghetto" class="misto" href="/mista/ghetto/ghetto.html">Ghetto</a>
+                <a target="_self" alt="Les Gost" class="misto" href="/mista/gost/gost.html">Gost</a> 
+                <a target="_self" alt="Grabštejn" class="misto" href="/mista/grabstejn/grabstejn.html">Grabštejn</a>
+                <a target="_self" alt="Hayax" class="misto" href="/mista/hayax/hayax.html">Hayax</a>
+                <a target="_self" alt="Hornická Oblast" class="misto" href="/mista/doly/doly.html">Hornická oblast</a>
+                <a target="_self" alt="Horská pevnost" class="misto" href="/mista/horska_pevnost/horska_pevnost.html">Horská pevnost</a>
+                <a target="_self" alt="Chvozd" class="misto" href="/mista/chvozd/chvozd.html">Chvozd</a>
+                <a target="_self" alt="Ječín" class="misto" href="/mista/jecin/jecin.html">Ječín</a>
+                <a target="_self" alt="Kriegberg" class="misto" href="/mista/kriegberg/kriegberg.html">Kriegberg</a>
+                <a target="_self" alt="Kriegstein" class="misto" href="/mista/kriegstein/kriegstein.html">Kriegstein</a>
+                <a target="_self" alt="Laréze" class="misto" href="/mista/lareze/lareze.html">Laréze</a>
+                <a target="_self" alt="Liedemar" class="misto" href="/mista/liedemar/liedemar.html">Liedemar</a>
+                <a target="_self" alt="Les Mrtvých Vesnic" class="misto" href="/mista/les_mrtvych_vesnic/les_mrtvych_vesnic.html">Les Mrtvých Vesnic</a>
+                <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz </a>
+                <a target="_self" alt="Morgstadt" class="misto" href="/mista/morgstadt/morgstadt.html">Morgstadt</a>
+                <a target="_self" alt="Mýtinná" class="misto" href="/mista/mytinna/mytinna.html">Mýtinná</a>
+                <a target="_self" alt="Pełzacz" class="misto" href="/mista/pelzacz/pelzacz.html">Pełzacz</a>
+                <a target="_self" alt="Pole Smutku" class="misto" href="/mista/pole_smutku/pole_smutku.html">Pole Smutku</a>
+                <a target="_self" alt="Pons Varoli" class="misto" href="/mista/pons/pons.html">Pons Varoli</a>
+                <a target="_self" alt="Přechod" class="misto" href="/mista/prechod/prechod.html">Přechod</a>
+                <a target="_self" alt="Pustina" class="misto" href="/mista/pustina/pustina.html">Pustina</a>
+                <a target="_self" alt="Sucholes" class="misto" href="/mista/sucholes/sucholes.html">Sucholes</a>
+                <a target="_self" alt="Trombus" class="misto" href="/mista/trombus/trombus.html">Trombus</a>
+                <a target="_self" alt="Trpasličí Zátoka" class="misto" href="/mista/zatoka/zatoka.html">Trpasličí Zátoka</a>
+                <a target="_self" alt="Varka" class="misto" href="/mista/varka/varka.html">Varka</a>
+                <a target="_self" alt="Věž" class="misto" href="/mista/vez/vez.html">Věž</a>
+                <a target="_self" alt="Všehostrom" class="misto" href="/mista/vsehostrom/vsehostrom.html">Všehostrom</a>
+            </div>
+            <br>
+
+            <button class="dropdown-btn" onclick="rozbal('div3')">Frakce
+                <span><i class="arrow down"></i></span>
+            </button>
+            <hr class="solid">
+            <div class="dropdown-div" style="visibility:hidden;max-height:0px" id="div3">
+                <a target="_self" alt="Aloca" class="misto" href="/frakce/aloca.html">Aloca</a>
+                <a target="_self" alt="Alchymistický cech" class="misto" href="/frakce/alchym_cech.html">Alchymistický cech</a>
+                <a target="_self" alt="bratrstvo lovu" class="misto" href="/frakce/lov.html">Bratrstvo lovu</a>
+                <a target="_self" alt="stráž garagoru" class="misto" href="/frakce/straz.html">Gargorská stráž</a>
+                <a target="_self" alt="GGG" class="misto" href="/frakce/ggg.html">Gargorský Gang Gaunerů</a>
+                <a target="_self" alt="Kult očistného plamene" class="misto" href="/frakce/kultop.html">Kult Očistného Plamene</a>
+                <a target="_self" alt="Stříbro" class="misto" href="/frakce/stribro.html">Stříbrné lemování</a>
+                <a target="_self" alt="Stráž Al-katu" class="misto" href="/frakce/straze_al-katu.html">Stráž Al-Katu</a>
+            </div>
+            <br>
+
+            <button class="dropdown-btn" onclick="rozbal('div4')">Bohové 
+              <span><i class="arrow down"></i></span>
+            </button>
+            <hr class="solid">
+            <div class="dropdown-div" id="div4" style="visibility:hidden;max-height:0px">
+            <a href="/bohove/adanos.html" class="postava">Adanos</a>
+            <a href="/bohove/bane.html" class="postava">Bane</a>
+            <a href="/bohove/beliar.html" class="postava">Beliar</a>
+            <a href="/bohove/bhaal.html" class="postava">Bhaal</a>
+            <a href="/bohove/ilmater.html" class="postava">Ilmater</a>
+            <a href="/bohove/innos.html" class="postava">Innos</a>
+            <a href="/bohove/magran.html" class="postava">Magran</a>
+            <a href="/bohove/myrkul.html" class="postava">Myrkul</a>
+            <a href="/bohove/posledni.html" class="postava">Ten Poslední</a>
+            <a href="/bohove/slepa.html" class="postava">Slepá</a>
+            <a href="/bohove/tlem.html" class="postava">Tlem</a>
+            <a href="/bohove/woedika.html" class="postava">Woedika</a>
+            <a href="/bohove/zkrouceny.html" class="postava">Zkroucený</a>
+            <a href="/bohove/panna.html" class="postava">Žulová Panna</a>   
+            </div>
+            <br>
+
+            <button class="dropdown-btn" onclick="rozbal('divSpell')">Spellbook
+                <span><i class="arrow down"></i></span>
+            </button>
+            <hr class="solid">
+            <div class="dropdown-div" id="divSpell" style="visibility:hidden;max-height:0px">
+              <a href="https://dnd5e.wikidot.com/spells:druid" class="postava">Druid</a>
+              <a href="https://dnd5e.wikidot.com/spells:cleric" class="postava">Klerik</a>  
+              <a href="https://dnd5e.wikidot.com/spells:paladin" class="postava">Paladin</a>
+              <a href="https://dnd5e.wikidot.com/spells:sorcerer" class="postava">Sorcerer</a>  
+              <a href="https://dnd5e.wikidot.com/spells:ranger" class="postava">Ranger</a> 
+              <a href="/Spellbook/fighterSb.html" class="postava">Válečník</a> 
+            </div>
+            <br>
+            <a href="/info.html" class="postava">O stránce</a> 
+            `;
+  console.log(document.getElementById("SIDE"));
+
+  let MortName = document.getElementById("mort");
+  console.log(MortName);
+
+  MortName.addEventListener("mouseover", () => {
+    MortName.textContent = "Mort";
+  });
+
+  MortName.addEventListener("mouseout", () => {
+    MortName.textContent = "Chloe";
+  });
+
+  document.querySelectorAll(".dropdown-btn").forEach((d) => {
+    d.addEventListener("click", () => {
+      const icon = d.querySelector("i.arrow");
+      if (icon) {
+        icon.classList.toggle("down");
+        icon.classList.toggle("up");
+      }
+    });
+  });
+}
+
+function rozbal(idDivu) {
+  const tenDiv = document.getElementById(idDivu);
+  let aktVisibility = getComputedStyle(tenDiv).visibility;
+
+  if (aktVisibility === "hidden") {
+    tenDiv.style.visibility = "visible";
+    tenDiv.style.maxHeight = "2000px";
+  } else {
+    tenDiv.style.visibility = "hidden";
+    tenDiv.style.maxHeight = "0px";
+  }
+}
