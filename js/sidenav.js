c9fe75bb83187b35b8dbc1f1f3de69098e8afcf4
aikinnothir <jindra.aikin@gmail.com>
2026-02-20 16:15:36 +0100

    odebrán zbytečný import lightboxu, který se nepoužívá začínám timeline

--- js/sidenav.js
+++ js/sidenav.js
@@ -208,6 +208,7 @@ function napisSidebar() {
             <br>
             <a href="/mapa.html" class="mapa">Interaktivní Mapa</a>
             <a href="/mapabig.html" class="mapa">VELKÁ MAPA!</a>
+            <a href="/mapabig.html" class="mapa">4</a>
             <a href="/cheatsheet.html" class="mapa">Cheatsheet</a>
             <a href="/denik.pdf" class="mapa" download>Deník!</a>
             <br>
